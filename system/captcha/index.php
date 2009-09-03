ec395d4dc40da3fc24fe971a40954990c9555b7d
Alex <konteck@gmail.com>
2009-09-03 17:11:50 +0000

    Added captcha related stuff

--- system/captcha/index.php
+++ system/captcha/index.php
@@ -2,8 +2,16 @@
 
 class QCaptcha
 {
+    private $width = 200;
+    private $height = 70;
+    
     function __construct()
     {
 
+    }
+
+    public function Render()
+    {
+        
     }
 }
